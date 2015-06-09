0409e66cb5147ab268166ba1de23c631ea51e96c
zismailov <zismailov@esphere.ru>
2015-06-09 16:01:49 +0300

    Small fix

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 /**
  * Created by zauri_000 on 08.06.2015.
  */
-window.onload = function () {
-    var elem1 = document.getElementById("first-widget");
-    var w1 = window.uploadWidget.create(elem1);
-    var elem2 = document.getElementById("second-widget");
-    var w2 = window.uploadWidget.create(elem2);
-    var w1ParentElement = w1.element.parentElement;
-    var w2ParentElement = w2.element.parentElement;
-    var w1Result = w1ParentElement.querySelector(".result");
-    var w2Result = w2ParentElement.querySelector(".result");
+window.onload = function() {
+    var w1 = window.uploadWidget.create(document.getElementById("first-widget")),
+        w2 = window.uploadWidget.create(document.getElementById("second-widget")),
+        w1ParentElement = w1.element.parentElement,
+        w2ParentElement = w2.element.parentElement,
+        w1Result = w1ParentElement.querySelector(".result"),
+        w2Result = w2ParentElement.querySelector(".result");
 
     w1ParentElement.querySelector(".serialize").addEventListener("click", function(e) {
         e.preventDefault();
